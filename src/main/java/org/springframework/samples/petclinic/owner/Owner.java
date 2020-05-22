a553f42cd321b660554f3019c74b4b708b323d94
yeoseon <devyyskr@gmail.com>
2020-05-22 09:01:54 +0900

    refactor(Owner): Owner에서 age라는 추가 요소를 관리할 수 있도록 처리

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -15,24 +15,15 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotEmpty;
-
 import org.springframework.beans.support.MutableSortDefinition;
 import org.springframework.beans.support.PropertyComparator;
 import org.springframework.core.style.ToStringCreator;
 import org.springframework.samples.petclinic.model.Person;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import java.util.*;
 
 /**
  * Simple JavaBean domain object representing an owner.
@@ -62,7 +53,21 @@ public class Owner extends Person {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets;
 
-	public String getAddress() {
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer age;
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getAddress() {
 		return this.address;
 	}
 
