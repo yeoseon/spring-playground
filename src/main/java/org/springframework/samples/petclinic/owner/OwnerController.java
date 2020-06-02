1690229ec77a2ca927724f98f4df48b87b4e1d23
yeoseon <devyyskr@gmail.com>
2020-06-03 08:35:05 +0900

    feat(proxy): 프록시패턴 예제

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StopWatch;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,8 +60,14 @@ class OwnerController {
 
     @GetMapping("/owners/new")
     public String initCreationForm(Map<String, Object> model) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         Owner owner = new Owner();
         model.put("owner", owner);
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
         return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
     }
 
