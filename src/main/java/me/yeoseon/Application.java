22984568b7b38ad4c93c213a3a9a5d2064e0cf7a
yeoseon <devyyskr@gmail.com>
2020-06-17 08:13:39 +0900

    feat(Application): @SpringBootApplication의 구성

--- src/main/java/me/yeoseon/Application.java
+++ src/main/java/me/yeoseon/Application.java
@@ -1,11 +1,18 @@
 package me.yeoseon;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication은 아래 3개의 어노테이션으로 구성된다.
+//* ```@SpringBootConfiguration```
+//* ```@ComponentScan```
+//* ```@EnableAutoConfiguration```
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
     }
 }
