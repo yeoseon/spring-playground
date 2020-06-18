5ea1434cabfd9039d0d3518570e25bb54538a42f
yeoseon <devyyskr@gmail.com>
2020-06-19 08:57:13 +0900

    feat(Holoman): Spring Custom Configuration 만들기

---
+++ src/main/java/me/yeoseon/HolomanConfiguration.java
@@ -0,0 +1,17 @@
+package me.yeoseon;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanConfiguration {
+
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(5);
+        holoman.setName("Yeoseon");
+
+        return holoman;
+    }
+}
