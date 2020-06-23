014b3bcd0f273068a9471d33966c9bdcbe32e95c
yeoseon <devyyskr@gmail.com>
2020-06-24 08:31:08 +0900

    Application Clear

--- src/main/java/me/yeoseon/Application.java
+++ src/main/java/me/yeoseon/Application.java
@@ -1,19 +1,6 @@
 package me.yeoseon;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-@SpringBootApplication
-// @SpringBootApplication은 아래 3개의 어노테이션으로 구성된다.
-//* ```@SpringBootConfiguration```
-//* ```@ComponentScan```
-//* ```@EnableAutoConfiguration```
 public class Application {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
     }
 }
