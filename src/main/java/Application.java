08b1a50279076e5144b651ab3682987fe63eee6a
yeoseon <devyyskr@gmail.com>
2020-06-29 08:53:49 +0900

    feat(Application): 예시 Contrller 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,18 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+@SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Spring";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
