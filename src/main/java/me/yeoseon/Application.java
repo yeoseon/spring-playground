efb40b6ac4d250fe7775c91a56087770fecb3456
yeoseon <devyyskr@gmail.com>
2020-07-03 09:07:27 +0900

    fix(Application): Default Package Setting

--- src/main/java/me/yeoseon/Application.java
+++ src/main/java/me/yeoseon/Application.java
@@ -1,3 +1,5 @@
+package me.yeoseon;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
