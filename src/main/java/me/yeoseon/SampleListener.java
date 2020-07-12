7444e00b7a23e4e11ce49eb769848fb55f4e22e4
yeoseon <devyyskr@gmail.com>
2020-07-13 08:31:25 +0900

    feat(SampleLiestner): ApplicationListener 등록

---
+++ src/main/java/me/yeoseon/SampleListener.java
@@ -0,0 +1,16 @@
+package me.yeoseon;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+// ApplicationListener의 타입을 정해주어야 한다.
+@Component
+public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        System.out.println("==================");
+        System.out.println("Application Starting");
+        System.out.println("==================");
+    }
+}
