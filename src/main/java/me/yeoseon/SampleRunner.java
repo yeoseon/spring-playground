fd5f0462ea3dddb45e303bff9644d2fc8234d115
yeoseon <devyyskr@gmail.com>
2020-07-30 08:58:26 +0900

    feat(SampleRunner): 외부설정 파일인 properties 설정을 사용

--- src/main/java/me/yeoseon/SampleRunner.java
+++ src/main/java/me/yeoseon/SampleRunner.java
@@ -1,4 +1,20 @@
 package me.yeoseon;
 
-public class SampleRunner {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+
+    @Value("${yeoseon.name}")
+    private String name;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("===============");
+        System.out.println(name);
+        System.out.println("===============");
+    }
 }
