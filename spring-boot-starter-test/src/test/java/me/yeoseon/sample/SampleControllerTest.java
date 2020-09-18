769a0f6e9a1fb4fe4779cf272396ca4179cb051a
Yeoseon Yun <devyyskr@gmail.com>
2020-09-18 11:40:27 +0900

    test(SampleControllerTest): TestRestTemplate의 테스트 스코프를 좁히고, Service를 interaction하기 위해 @MockBean을 추가했다. 관련하여 mokito 설정도 추가했다.

--- spring-boot-starter-test/src/test/java/me/yeoseon/sample/SampleControllerTest.java
+++ spring-boot-starter-test/src/test/java/me/yeoseon/sample/SampleControllerTest.java
@@ -5,22 +5,28 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 public class SampleControllerTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("celine");
+
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello yeoseon");
+        assertThat(result).isEqualTo("hello celine");
     }
 }
