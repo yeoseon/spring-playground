92ff9b332f5c467e1491a68156b5b81e3ac7ad53
Yeoseon Yun <devyyskr@gmail.com>
2020-09-18 11:51:34 +0900

    test(SampleControllerTest): Async 방식의 WebTestClient를 이용하여 테스트하도록 수정

--- spring-boot-starter-test/src/test/java/me/yeoseon/sample/SampleControllerTest.java
+++ spring-boot-starter-test/src/test/java/me/yeoseon/sample/SampleControllerTest.java
@@ -3,11 +3,10 @@ package me.yeoseon.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -17,7 +16,7 @@ import static org.mockito.Mockito.when;
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -26,7 +25,8 @@ public class SampleControllerTest {
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("celine");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello celine");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello celine");
     }
 }
