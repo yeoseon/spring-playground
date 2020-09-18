944b85121b3cf9e3470781cd852eda65327734da
Yeoseon Yun <devyyskr@gmail.com>
2020-09-18 11:11:19 +0900

    SampleControllerTest): 1. Springboot Test 수행을 위한 기본 메소드 및 구조 추가. 2. MockMVC를 이용한 테스트코드 추가

--- spring-boot-starter-test/src/main/java/me/yeoseon/Application.java
+++ spring-boot-starter-test/src/main/java/me/yeoseon/Application.java
@@ -2,7 +2,9 @@ package me.yeoseon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
---
+++ spring-boot-starter-test/src/main/java/me/yeoseon/sample/SampleService.java
@@ -0,0 +1,11 @@
+package me.yeoseon.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "yeoseon";
+    }
+}
