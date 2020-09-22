bdb945967e55d63bc5126d4c921c420f4d361ea5
Yeoseon Yun <devyyskr@gmail.com>
2020-09-22 10:20:56 +0900

    test(UserControllerTest): createUser_JSON 테스트 코드 작성

--- demospringmvc/src/test/java/me/yeoseon/user/UserControllerTest.java
+++ demospringmvc/src/test/java/me/yeoseon/user/UserControllerTest.java
@@ -4,12 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.awt.*;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -23,5 +28,18 @@ public class UserControllerTest {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson = "";
+
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(userJson))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.username", is(equalTo("yeoseon"))))
+                    .andExpect(jsonPath("$.password", is(equalTo("123"))));
     }
 }
