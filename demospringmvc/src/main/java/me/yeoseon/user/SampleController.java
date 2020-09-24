3e373f1ccc04474c3927e0d1ed9afb1445ec1d5b
Yeoseon Yun <devyyskr@gmail.com>
2020-09-24 10:37:01 +0900

    test(SampleControllerTest): Template engine 사용을 위한 테스트 코드 작성

---
+++ demospringmvc/src/main/java/me/yeoseon/user/SampleController.java
@@ -0,0 +1,8 @@
+package me.yeoseon.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleController {
+
+}
---
+++ demospringmvc/src/test/java/me/yeoseon/user/SampleControllerTest.java
@@ -0,0 +1,28 @@
+package me.yeoseon.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("yeoseon")));      // 모델 이름 : yeoseon, 뷰 이름 : hello
+    }
+}
