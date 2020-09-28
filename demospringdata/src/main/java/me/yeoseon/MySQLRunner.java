d2cff223ed3b85d97978a34787c6adefcf582d27
Yeoseon Yun <devyyskr@gmail.com>
2020-09-28 10:56:14 +0900

    feat(MySQLRUnneR): MySQL 접속 예시 추가

--- demospringdata/src/main/java/me/yeoseon/MySQLRunner.java
+++ demospringdata/src/main/java/me/yeoseon/MySQLRunner.java
@@ -9,12 +9,10 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.Statement;
 
 @Component
-@Order(1)
-public class H2Runner implements ApplicationRunner {
-
+@Order(2)
+public class MySQLRunner implements ApplicationRunner {
     // JDBC 의존성 추가시 아래 2개의 객체를 바로 사용할 수 있다.
     @Autowired
     DataSource dataSource;
@@ -25,15 +23,19 @@ public class H2Runner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try(Connection connection = dataSource.getConnection()) {
-            System.out.println("[H2 Database Configuration]");
+            System.out.println("[MySQL Database Configuration]");
+            System.out.println(dataSource.getClass());  // 어떤 DBCP를 사용하는지 보기 위해
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
             System.out.println("-----------------------------------------------------------------");
             //원래는 Transaction 처리를 해줘야 한다.
             //테이블 생성
 //            Statement statement = connection.createStatement();
-////            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
-////            statement.executeUpdate(sql);
+//            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+//            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
         }
 
         // jdbcTamplate을 사용할 경우 위의 많은 줄을 아래와 같이 간결하고 안전하게 (try-catch 처리가 알아서 됨) sql을 사용할 수 있다.
