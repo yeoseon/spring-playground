cd615e9ffba0c41fdce52f779af1ddc618748e20
Yeoseon Yun <devyyskr@gmail.com>
2020-09-29 11:20:30 +0900

    feat(AccountRepository): SQL을 병행하여 사용하고 싶을 때 설정 (nativeQuery)

--- demospringdata/src/main/java/me/yeoseon/account/AccountRepository.java
+++ demospringdata/src/main/java/me/yeoseon/account/AccountRepository.java
@@ -1,8 +1,11 @@
 package me.yeoseon.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query(nativeQuery = true, value = "select * from account where username = '{0}'")
     Account findByUsername(String username);   // Long: id의 타입
 
 }
