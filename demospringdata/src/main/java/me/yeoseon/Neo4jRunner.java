3964518ee7fcb677b3be8ae3084fe4671bf2d710
Yeoseon Yun <devyyskr@gmail.com>
2020-10-12 10:36:07 +0900

    feat(neo4j): neo4j 사용 예시 추가

---
+++ demospringdata/src/main/java/me/yeoseon/Neo4jRunner.java
@@ -0,0 +1,28 @@
+package me.yeoseon;
+
+import me.yeoseon.neo4jaccount.Account;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Neo4jRunner implements ApplicationRunner {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+
+        account.setEmail("devyyskr@gmail.com");
+        account.setUserName("yeoseon");
+
+        Session session = sessionFactory.openSession();
+        session.save(account);
+        sessionFactory.close();
+    }
+}
---
+++ demospringdata/src/main/java/me/yeoseon/neo4jaccount/Account.java
@@ -0,0 +1,39 @@
+package me.yeoseon.neo4jaccount;
+
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
+public class Account {
+
+    @Id
+    private Long id;        // Neo4j는 String 에는 @Id를 붙힐 수 없다.
+
+    private String userName;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
