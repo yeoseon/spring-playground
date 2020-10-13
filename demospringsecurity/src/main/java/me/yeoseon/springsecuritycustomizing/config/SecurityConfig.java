7ca3d959445d2c4dac62c05811d42365cf838842
Yeoseon Yun <devyyskr@gmail.com>
2020-10-13 11:14:39 +0900

    feat(PasswordEncoder): Password Encoder 설정 (NoOp)

--- demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/config/SecurityConfig.java
+++ demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package me.yeoseon.springsecuritycustomizing.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,5 +20,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
