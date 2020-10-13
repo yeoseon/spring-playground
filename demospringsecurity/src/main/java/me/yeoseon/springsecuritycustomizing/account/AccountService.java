a23fe2b07b235cac1edabbaeccfeb7d9b6e81bdd
Yeoseon Yun <devyyskr@gmail.com>
2020-10-13 11:15:53 +0900

    feat(PasswordEncoder): Spring에서 권장하는 Password Encoder 설정

--- demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/account/AccountService.java
+++ demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/account/AccountService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -20,11 +21,14 @@ public class AccountService implements UserDetailsService {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Account createAccount(String username, String password) {
         Account account = new Account();
 
         account.setUsername(username);
-        account.setPassword(password);
+        account.setPassword(passwordEncoder.encode(password));
 
         return accountRepository.save(account);
     }
--- demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/config/SecurityConfig.java
+++ demospringsecurity/src/main/java/me/yeoseon/springsecuritycustomizing/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -24,6 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
