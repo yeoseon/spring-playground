9dcaa69bfeeb60877cd2912e94d43ba2b58324ef
Yeoseon Yun <devyyskr@gmail.com>
2020-10-13 09:17:41 +0900

    feat(HomeController, WebConfig): url에 맞는 view 이름을 호출하고 싶을 때의 2가지 방법 예제 추가

---
+++ demospringsecurity/src/main/java/me/yeoseon/springbootsecurity/HomeController.java
@@ -0,0 +1,13 @@
+package me.yeoseon.springbootsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
