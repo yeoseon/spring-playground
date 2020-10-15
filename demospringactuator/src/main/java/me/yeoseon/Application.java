c7e61c532a01fe34c9215e6e3c803b526a5f6447
Yeoseon Yun <devyyskr@gmail.com>
2020-10-15 11:17:48 +0900

    feat(Actuator): Actuator 설정

--- demospringactuator/src/main/java/me/yeoseon/Application.java
+++ demospringactuator/src/main/java/me/yeoseon/Application.java
@@ -2,9 +2,16 @@ package me.yeoseon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
